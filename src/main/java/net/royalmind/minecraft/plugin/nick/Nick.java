bef41abc8fa965a418703898c4357fb1a2613e34
Dali <oscardali370@gmail.com>
2021-07-08 18:43:29 -0500

    Initial commit

---
+++ src/main/java/net/royalmind/minecraft/plugin/nick/Nick.java
@@ -0,0 +1,17 @@
+package net.royalmind.minecraft.plugin.nick;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Nick extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
