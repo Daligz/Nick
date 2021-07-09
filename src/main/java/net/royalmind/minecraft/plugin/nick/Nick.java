a267f42f26cba1ef71ee722af63035ee7d3a382c
Dali <oscardali370@gmail.com>
2021-07-08 20:16:08 -0500

    Nick command implemented

--- src/main/java/net/royalmind/minecraft/plugin/nick/Nick.java
+++ src/main/java/net/royalmind/minecraft/plugin/nick/Nick.java
@@ -1,17 +1,41 @@
 package net.royalmind.minecraft.plugin.nick;
 
+import net.md_5.bungee.api.ChatMessageType;
+import net.md_5.bungee.api.chat.TextComponent;
+import net.royalmind.minecraft.plugin.nick.commands.NickCommand;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitRunnable;
+import xyz.haoshoku.nick.api.NickAPI;
+
+import java.util.UUID;
 
 public final class Nick extends JavaPlugin {
 
     @Override
     public void onEnable() {
         // Plugin startup logic
-
+        this.getCommand("nick").setExecutor(new NickCommand());
+        this.messageService();
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+    }
+
+    private void messageService() {
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                for (final UUID uuid : NickAPI.getNickedPlayers().keySet()) {
+                    final Player player = Bukkit.getPlayer(uuid);
+                    if (player == null) return;
+                    if (!(player.isOnline())) return;
+                    player.spigot().sendMessage(ChatMessageType.ACTION_BAR, TextComponent.fromLegacyText("Currently you are Nicked"));
+                }
+            }
+        }.runTaskTimer(this, 0L, 20L);
     }
 }
---
+++ src/main/java/net/royalmind/minecraft/plugin/nick/commands/NickCommand.java
@@ -0,0 +1,60 @@
+package net.royalmind.minecraft.plugin.nick.commands;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import xyz.haoshoku.nick.api.NickAPI;
+
+public class NickCommand implements CommandExecutor {
+
+    private static final String[] HELP_MESSAGES = {
+            "/nick uui",
+            "/nick off",
+            "/nick <Player>",
+    };
+
+    @Override
+    public boolean onCommand(final CommandSender sender, final Command command, final String label, final String[] args) {
+        if (!(sender instanceof Player)) {
+            sender.sendMessage("No puedes ejecutar este comando en consola!");
+            return true;
+        }
+        final Player player = (Player) sender;
+        if (!(player.hasPermission("royalmind.nick.use"))) {
+            player.sendMessage(ChatColor.RED + "No tienes permiso de ejecutar este comando!");
+            return true;
+        }
+        if (args.length <= 0) {
+            for (final String helpMessage : HELP_MESSAGES) {
+                player.sendMessage(ChatColor.YELLOW + helpMessage);
+            }
+            return true;
+        }
+        final String arg = args[0];
+        if (arg.equalsIgnoreCase("off")) {
+            NickAPI.resetNick(player);
+            NickAPI.resetGameProfileName(player);
+            NickAPI.resetSkin(player);
+            NickAPI.resetUniqueId(player);
+            player.sendMessage(ChatColor.YELLOW + "Vuelves a ser " + player.getDisplayName());
+            return true;
+        } else if (arg.equalsIgnoreCase("uuid")) {
+            player.sendMessage(ChatColor.RED + "Tu UUID es : " + player.getUniqueId());
+            return true;
+        }
+        if (NickAPI.isNicked(player)) {
+            player.sendMessage(ChatColor.RED + "Ya tienes un nick!");
+            return true;
+        }
+
+        NickAPI.nick(player, arg);
+        NickAPI.setSkin(player, arg);
+        NickAPI.setUniqueId(player, arg);
+        NickAPI.setGameProfileName(player, arg);
+        NickAPI.refreshPlayer(player);
+        player.sendMessage(ChatColor.GREEN + "Ahora eres " + arg);
+        return true;
+    }
+}
